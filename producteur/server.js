2a4545c539554cbeeb49f0a701449dc7d7b10f89
Akashos23 <65364908+Akashos23@users.noreply.github.com>
2024-02-06 19:39:52 +0100

    Étape 2 – Star de Cinéma : « 26l / 100 km » - Qui suis-je ?

--- producteur/server.js
+++ producteur/server.js
@@ -1,16 +1,5 @@
-import * as Admin from "../src/redpanda/admin.js"
-import * as Producer from "./producers.js"
-import * as Consommeur from './Consommeur.js'
-import {getUser} from "../src/messages/userlist.js";
-import {getStringMessage} from "../src/messages/stringmessages.js";
-import {getNumberMessage} from "../src/messages/numbermessage.js";
-import {getConfigNumber, getDebug, getNumberWord, getTimeOut, getTopic, getTypeMessage} from "../src/config/config.js";
 
-const configNumber = getConfigNumber()
-const typeMessage = getTypeMessage()
-const topic = getTopic()
-const numberWorld = getNumberWord()
-const debug = getDebug()
+import * as Consommeur from './Consommeur.js'
 
 async function start() {
 
